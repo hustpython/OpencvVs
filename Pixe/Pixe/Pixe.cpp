ff56b80d9d1b62e3c578c0c35323a128ed281f23
mxq <lgcrqyqx@sina.com>
2018-06-21 12:34:58 +0800

    s

---
+++ Pixe/Pixe/Pixe.cpp
@@ -0,0 +1,20 @@
+// Pixe.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <opencv2\opencv.hpp>
+using namespace std;
+using namespace cv;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Mat m(400, 400, CV_8U, Scalar(0));
+	for (int col = 0; col < 400; col++)
+	{
+		for (int row = 195; row < 205; row++)
+		{
+			cout << (int)(*(m.data + m.step[0]))
+		}
+	}
+	return 0;
+}
+
